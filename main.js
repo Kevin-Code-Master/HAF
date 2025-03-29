fa8f7d04534d1239b612d88f69940ada042abef8
Kevin-Code-Master <koluoch28@gmail.com>
2025-03-29 11:55:29 +0300

    Structure the page much better

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("Hello world");
+
+
+const dob = document.getElementById("dob").value;
+
+function calculateAge() {
+    
+    let patientDob = new Date(dob);
+    let today = new Date();
+    let patientAge = today.getFullYear() - patientDob.getFullYear();
+
+    let monthDiff = today.getMonth() - patientDob.getMonth();
+    let dayDiff = today.getDate() - patientDob.getDate();
+
+    // adjust age if birthday has not occured yet this year
+
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        patientAge--;
+    }
+    return patientAge;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    let dobField = document.getElementById("dob");
+    let ageSpan = document.getElementById("age");
+
+    if (dobField) {
+        dobField.addEventListener("change", () => {
+            ageSpan.textContent = calculateAge();
+        });
+    };
+});
